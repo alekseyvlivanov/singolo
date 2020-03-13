2d9501bd6e58a83633804b9eabeac569552031e9
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-03-14 00:35:19 +1000

    feat: add active portfolio image switching

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ menu.addEventListener('click', event => {
     .forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
 });
+
+// Portfolio images
+const portfolio = document.querySelector('.portfolio-grid');
+
+portfolio.addEventListener('click', event => {
+  portfolio
+    .querySelectorAll('.portfolio-item img')
+    .forEach(el => el.classList.remove('active'));
+  event.target.classList.add('active');
+});
