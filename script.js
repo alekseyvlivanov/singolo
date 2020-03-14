8960d4231a24e613f14b01b73e1acb1f52cc1084
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-03-14 23:28:45 +1000

    feat: add contact form modal message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Header menu
+// Header
 const menu = document.querySelector('.menu');
 
 menu.addEventListener('click', event => {
@@ -8,7 +8,7 @@ menu.addEventListener('click', event => {
   event.target.classList.add('active');
 });
 
-// Portfolio images
+// Portfolio. Взаимодействие с картинками
 const portfolio = document.querySelector('.portfolio-grid');
 
 portfolio.addEventListener('click', event => {
@@ -16,4 +16,36 @@ portfolio.addEventListener('click', event => {
     .querySelectorAll('.portfolio-item img')
     .forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
+});
+
+// Get a quote
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', event => {
+  event.preventDefault();
+  if (contactForm.checkValidity()) {
+    const contactSubject = document
+      .getElementById('contact-subject')
+      .value.trim();
+    const contactMessage = document
+      .getElementById('contact-message')
+      .value.trim();
+
+    document.getElementById('info-subject').innerText = contactSubject
+      ? 'Тема: ' + contactSubject
+      : 'Без темы';
+    document.getElementById('info-text').innerText = contactMessage
+      ? 'Описание: ' + contactMessage
+      : 'Без описания';
+    document.getElementById('info-block').style.display = 'block';
+  }
+  contactForm.reset();
+});
+
+const okButton = document.getElementById('ok-button');
+
+okButton.addEventListener('click', () => {
+  document.getElementById('info-block').style.display = 'none';
+  document.getElementById('info-subject').innerText = '';
+  document.getElementById('info-text').innerText = '';
 });
